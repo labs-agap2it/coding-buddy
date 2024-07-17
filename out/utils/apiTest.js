2f275dd08d41c5f41b563cf94aea441a622ee73d
Ricardo Pinto <ricardoschillermail@gmail.com>
2024-07-17 16:37:06 +0100

    Removed out from repository

--- out/utils/apiTest.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const openai_1 = require("openai");
-const openai = new openai_1.OpenAI();
-//# sourceMappingURL=apiTest.js.map
